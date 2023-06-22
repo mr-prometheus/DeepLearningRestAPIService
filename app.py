73e284a602ef4a232a97fb752f3791d566ad048b
mr-prometheus <deepuadak@gmail.com>
2023-06-23 03:23:39 +0530

    Changes

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory,flash
-import os
-
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
@@ -10,20 +7,12 @@ from keras.utils import load_img, img_to_array
 from keras.applications.mobilenet import MobileNet, preprocess_input
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 from pickle import NONE
 import numpy as np
 import cv2
-from PIL import Image
 from flask_cors import CORS,cross_origin
 app = Flask(__name__)
 CORS(app,origins = ["https://even-morefruitsfrontend.vercel.app/"])
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-UPLOAD_FOLDER = 'static/uploads/'
-parent_dir = 'static/uploads/'
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 MODEL_PATH = 'fruits.h5'
 model = load_model(MODEL_PATH)      
@@ -37,42 +26,6 @@ def home():
 def hello():
     return "Hello"
 
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-   
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        user = request.form["fname"]
-        
-        #print(file.filename)
-        #filename = secure_filename(file.filename)
-        if not os.path.exists(os.path.join(parent_dir, user)):
-
-            path = os.path.join(parent_dir, user)
-            os.mkdir(path)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{user}/{user}.jpg"))
-            #print('upload_image filename: ' + filename)
-            user = f"{user}.jpg"
-            flash('Image saved successfully')
-            return render_template('index.html')
-        else:
-            flash("User already Present")
-            return redirect(request.url)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
-
-
 @app.route('/predict', methods=['GET', 'POST'])
 @cross_origin(allow_headers=['Content-Type'])
 def upload():
@@ -85,8 +38,6 @@ def upload():
         img = cv2.resize(img,(224,224))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         x = img_to_array(img)
-        print(x[4])    
-        # x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
 
    
@@ -94,7 +45,6 @@ def upload():
         output = np.argmax(prediction)
         predicted_class = CLASS_NAMES[output]
         max_prob = np.max(prediction)
-        print(max_prob)
         if max_prob >0.75:
             return {
             'class' : predicted_class,
